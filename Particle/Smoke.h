1bcb480e493af62af84c6aa87a0a19dcc8a19fc6
LC1B_04_Izumida_Masato <k021g1329@g.neec.ac.jp>
2022-09-08 13:52:43 +0900

    Smokeの追加

---
+++ Particle/Smoke.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class Smoke {
+
+public:
+
+	/// <summary>
+	/// ‰Šú‰»
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// –ˆƒtƒŒ[ƒ€ˆ—
+	/// </summary>
+	void Update(char* keys, char* oldkeys);
+
+	/// <summary>
+	/// •`‰æ
+	/// </summary>
+	void Draw();
+
+
+private:
+
+
+
+};
---
+++ Smoke.h
@@ -0,0 +1,27 @@
+#pragma once
+#pragma once
+
+class Smoke {
+public:
+
+	/// <summary>
+	/// ‰Šú‰»
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// –ˆƒtƒŒ[ƒ€ˆ—
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// •`‰æ
+	/// </summary>
+	void Draw();
+
+
+private:
+
+
+
+};
