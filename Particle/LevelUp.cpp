39b23116914e7b8ee62d263b9e47e8a69ce3b68b
masato izumida <Oioi_0126@outlook.com>
2022-09-11 14:15:50 +0900

    LevelUp

---
+++ Particle/LevelUp.cpp
@@ -0,0 +1,13 @@
+#include "LevelUp.h"
+
+void LevelUp::Initialize()
+{
+}
+
+void LevelUp::Update()
+{
+}
+
+void LevelUp::Draw()
+{
+}
---
+++ Particle/LevelUp.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class LevelUp
+{
+public:
+
+	/// <summary>
+	/// ‰Šú‰»
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// –ˆƒtƒŒ[ƒ€ˆ—
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// •`‰æ
+	/// </summary>
+	void Draw();
+
+private:
+
+};
